50c169bbef4dc5d9ee5845348949bbe7cee87772
pinaralper <ppinaralper@gmail.com>
2023-08-29 15:21:43 +0300

    Initial Commit

---
+++ MyMapApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMapApp
+//
+//  Created by PINAR KALKAN on 29.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
